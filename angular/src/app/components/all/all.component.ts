48ab22b3a823b359468036565e637cb4617eec97
Milan Vukovic <mildzan@gmail.com>
2017-06-30 23:45:10 +0200

    expand client works

--- angular/src/app/components/all/all.component.ts
+++ angular/src/app/components/all/all.component.ts
@@ -15,7 +15,7 @@ import { trigger,
   styleUrls: ['./all.component.css'],
   
   animations: [
-    trigger('showing', [
+    trigger('clientState', [
     state('show', style({
       display:"block"
      
@@ -32,9 +32,14 @@ import { trigger,
 
 export class AllComponent implements OnInit {
 
-   result:{}
+  
    state: String = "hide"
-
+  
+   show:boolean = false
+    public result:Array<any> = [{
+    show: false
+  }]
+   
   constructor(
     private authService:AuthService,
     private router:Router,
@@ -44,9 +49,25 @@ export class AllComponent implements OnInit {
     this.authService.getAll()
     .subscribe(result => this.result = result)  
     // console.log("Success") 
+    
   }
 
-  toggleState() {
+  
+
+      clicked(client, index) {
+
+        console.log(index)
+              console.log(this.result[index].show)
+        this.result[index].show = !this.result[index].show;
+              console.log(this.result[index].show)
+      }
+        // this.show = !this.show
+
+      
+
+
+    toggleState(i) {
+      console.log(i)
       this.state = (this.state === 'show' ? 'hide' : 'show');
     }
   
