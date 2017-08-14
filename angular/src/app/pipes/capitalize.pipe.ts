17ad8c7665cfb08dbdca670f00e04d574c8ff0a1
Mildza <mildzan@gmail.com>
2017-08-14 15:51:32 +0200

    glyphicon

--- angular/src/app/pipes/capitalize.pipe.ts
+++ angular/src/app/pipes/capitalize.pipe.ts
@@ -3,13 +3,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({
   name: 'capitalize'
 }) 
-// export class CapitalizePipe implements PipeTransform { 
-//   transform(value:any) { 
-//    if (value) { 
-//     return value.charAt(0).toUpperCase() + value.slice(1); 
-//     } return value; 
-//   } 
-// }
+
 export class CapitalizePipe implements PipeTransform {
   transform(value: string, args: string[]): any {
     if (!value) return value;
--- angular/src/app/pipes/date.pipe.ts
+++ angular/src/app/pipes/date.pipe.ts
@@ -3,24 +3,13 @@ import * as moment from 'moment';
 @Pipe({
   name: 'mydate'
 }) 
-// export class CapitalizePipe implements PipeTransform { 
-//   transform(value:any) { 
-//    if (value) { 
-//     return value.charAt(0).toUpperCase() + value.slice(1); 
-//     } return value; 
-//   } 
-// }
+
 export class DatePipe implements PipeTransform {
    transform(value: string, args: string[]): any {
-    
-    
+  
     var datum = moment(value).format('L')
     
     return datum
 
-    // if (value) {
-    //         var date = value instanceof Date ? value : new Date(value);
-    //         return DateFormatter.format(date, 'pt', 'dd/MM/yyyy');
-    //     }
   }
 }
