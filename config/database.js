9e6e1e6f20540153df7416db68c9ec031b124f8a
mildzan@gmail.com <mildzan@gmail.com>
2020-10-17 23:46:56 +0200

    asd

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/generali',
-    
     // database: 'mongodb://mildza:mikirurk@ds153400.mlab.com:53400/nodetododb',
     database: 'mongodb://mildza:mikirurk@generali-shard-00-00.9mqdw.mongodb.net:27017,generali-shard-00-01.9mqdw.mongodb.net:27017,generali-shard-00-02.9mqdw.mongodb.net:27017/nodetododb?ssl=true&replicaSet=atlas-10vxye-shard-0&authSource=admin&retryWrites=true&w=majority',
     secret: 'tajna'
